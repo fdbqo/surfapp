8cd6d15d470aef2629259aa80ee43a60b379ecfa
Finn De Bie <finndebie30@gmail.com>
2025-03-25 17:09:10 +0000

    ui fixes - spot display

--- src/app/pages/surf-spot-list/surf-spot-list.component.ts
+++ src/app/pages/surf-spot-list/surf-spot-list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { ApiService } from '../../services/api.service';
+import { ApiService } from '@/app/services/api.service';
 import { MatCardModule } from '@angular/material/card';
 
 @Component({
@@ -8,6 +8,7 @@ import { MatCardModule } from '@angular/material/card';
   standalone: true,
   imports: [CommonModule, MatCardModule],
   templateUrl: './surf-spot-list.component.html',
+  styleUrls: ['./surf-spot-list.component.css']
 })
 export class SurfSpotListComponent implements OnInit {
   spots: any[] = [];
