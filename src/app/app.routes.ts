b40603370a3802d185722403a23323acbf6932c0
Finn De Bie <finndebie30@gmail.com>
2025-03-26 12:28:45 +0000

    build fix - fix test 1

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -7,6 +7,6 @@ import { AuthGuard } from './guards/auth.guard';
 export const routes: Routes = [
   { path: '', component: SurfSpotListComponent },
   { path: 'create', component: CreateSpotComponent, canActivate: [AuthGuard] },
-  { path: 'spots/:id', component: SpotDetailsComponent, canActivate: [AuthGuard] },
+  { path: 'spots/:id', component: SpotDetailsComponent, canActivate: [AuthGuard], loadChildren: () => import('@/app/components/spot-map/spot-map.component').then(m => m.SpotMapComponent) },
   { path: '**', redirectTo: '' }
 ];
