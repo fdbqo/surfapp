0b97ec71952aa13b7d8fdfe9a27a8ae37bad23f3
Finn De Bie <finndebie30@gmail.com>
2025-03-28 16:02:37 +0000

    route fix

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,6 +2,7 @@ import type { Routes } from "@angular/router"
 import { SurfSpotListComponent } from "./pages/surf-spot-list/surf-spot-list.component"
 import { CreateSpotComponent } from "./pages/create-spot/create-spot.component"
 import { SpotDetailsComponent } from "./pages/spot-details/spot-details.component"
+import { UserProfileComponent } from "./pages/user-profile/user-profile.component"
 import { AuthGuard } from "./guards/auth.guard"
 
 export const routes: Routes = [
@@ -9,6 +10,7 @@ export const routes: Routes = [
   { path: "create", component: CreateSpotComponent, canActivate: [AuthGuard] },
   { path: "edit/:id", component: CreateSpotComponent, canActivate: [AuthGuard] },
   { path: "spots/:id", component: SpotDetailsComponent, canActivate: [AuthGuard] },
+  { path: "profile", component: UserProfileComponent, canActivate: [AuthGuard] },
   { path: "**", redirectTo: "" },
 ]
 
